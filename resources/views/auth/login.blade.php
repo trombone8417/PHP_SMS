1df91d9109e1a3d20c879c881e215eb5659da056
trombone8417 <trombone8417@yahoo.com>
2021-04-15 07:48:49 +0800

    student template

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -35,7 +35,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
     <div class="login-logo">
-        <a href="{{ url('/home') }}"><b>InfyOm </b>Generator</a>
+        <a href="{{ url('/home') }}"><b>Admin / Teacher Login </b>Page</a>
     </div>
 
     <!-- /.login-logo -->
